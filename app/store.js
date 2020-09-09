8b7fcce730bbad89ad72d3008366c267432501b4
Jordan Rivers <j.russell.rivers@gmail.com>
2020-09-08 21:53:37 -0400

    added comments to react/redux

--- app/store.js
+++ app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import postsReducer from '../features/posts/postsSlice'
+import commentsReducer from '../features/comments/commentsSlice'
 
 export default configureStore({
   reducer: {
-    posts: postsReducer
+    posts: postsReducer,
+    comments: commentsReducer
   }
 })
 
