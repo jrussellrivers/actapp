f6c71a7bcbd7eb73b5a86b7864ff80d192247c2d
Dylan Stone-Miller <dylan@macbook-pro-3.attlocal.net>
2020-09-11 16:49:25 -0400

    image uploader not sending FormData back to backend

--- features/posts/AddPost.js
+++ features/posts/AddPost.js
@@ -36,32 +36,33 @@ export default function AddImage() {
                 quality: 1,
             });
             if (!result.cancelled) {
-                result.fileType = result.uri.substring(result.uri.lastIndexOf(".") + 1);
+                let localUri = result.uri;
+                let filename = localUri.split('/').pop();
+                // Infer the type of the image
+                let match = /\.(\w+)$/.exec(filename);
+                let type = match ? `image/${match[1]}` : `image`;
+                result.filename = filename
+                result.type = type
                 setImage(result);
             }
     
-            console.log(result.uri);
+            console.log(result);
         } catch (E) {
             console.log(E);
         }
     };
 
-    const _uploadToDB = (photo, body) => {
-        const data = new FormData();
-        photo = image
-        console.log(photo)
-        data.append("photo", {
-            name: photo.fileName,
-            type: photo.type,
-            uri:
-            Platform.OS === "android" ? photo.uri : photo.uri.replace("file://", "")
-        });
-        body = data
-        Object.keys(body).forEach(key => {
-            data.append(key, body[key]);
-        });
-        fetch('http://localhost:3333/upload',{method:'POST',body:data,headers:{"Content-Type": "multipart/form-data"}})
-    }
+    const _uploadToDB = async () => {
+            // Upload the image using the fetch and FormData APIs
+            let formData = new FormData();
+            // Assume "photo" is the name of the form field the server expects
+            formData.append('photo', { uri: image.localUri, name: image.filename, type: image.type });
+          
+            return await fetch('http://localhost:3333/upload', {
+              method: 'POST',
+              body: formData
+            });
+          }
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -72,52 +73,3 @@ export default function AddImage() {
         </View>
     )
 }
-
-
-// export default class ImagePickerExample extends React.Component {
-//   state = {
-//     image: null,
-//   };
-
-//   render() {
-//     let { image } = this.state;
-
-//     return (
-//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//         <Button title="Pick an image from camera roll" onPress={this._pickImage} />
-//         {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-//       </View>
-//     );
-//   }
-
-//   componentDidMount() {
-//     this.getPermissionAsync();
-//   }
-
-//   getPermissionAsync = async () => {
-//     if (Platform.OS !== 'web') {
-//       const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-//       if (status !== 'granted') {
-//         alert('Sorry, we need camera roll permissions to make this work!');
-//       }
-//     }
-//   };
-
-//   _pickImage = async () => {
-//     try {
-//       let result = await ImagePicker.launchImageLibraryAsync({
-//         mediaTypes: ImagePicker.MediaTypeOptions.All,
-//         allowsEditing: true,
-//         aspect: [4, 3],
-//         quality: 1,
-//       });
-//       if (!result.cancelled) {
-//         this.setState({ image: result.uri });
-//       }
-
-//       console.log(result);
-//     } catch (E) {
-//       console.log(E);
-//     }
-//   };
-// }
