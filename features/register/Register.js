38026f47f338d492ba8e334ba8877a7ed73c8f33
Dylan Stone-Miller <dylan@macbook-pro-3.attlocal.net>
2020-09-18 10:04:49 -0400

    updated register

--- features/register/Register.js
+++ features/register/Register.js
@@ -35,10 +35,6 @@ export default function Register() {
             <TextInput name="firstName" onChangeText={text => handleChange("firstName",text)} placeholder="First Name" />
             <TextInput name="lastName" onChangeText={text => handleChange("lastName",text)} placeholder="Last Name" />
             <TextInput name="email" onChangeText={text => handleChange("email",text)} placeholder="Email" />
-            <TextInput name="streetAddress" onChangeText={text => handleChange("streetAddress",text)} placeholder="Address" />
-            <TextInput name="city" onChangeText={text => handleChange("city",text)} placeholder="City" />
-            <TextInput name="state" onChangeText={text => handleChange("state",text)} placeholder="State" />
-            <TextInput name="zipcode" onChangeText={text => handleChange("zipcode",text)} placeholder="Zip Code" />
             <TextInput name="race" onChangeText={text => handleChange("race",text)} placeholder="Race/Ethnicity" />
             <TextInput name="gender" onChangeText={text => handleChange("gender",text)} placeholder="Identified Gender" />
             <TextInput name="birthdate" onChangeText={text => handleChange("birthdate",text)} placeholder="Birthdate" />
